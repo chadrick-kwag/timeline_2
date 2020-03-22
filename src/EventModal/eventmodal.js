c979c9daeab66798acfbbe0cc8051dc8bd121682
chadrick-kwag <chadrick.kwag@gmail.com>
2020-03-22 22:07:39 +0900

    add date to modal

--- src/EventModal/eventmodal.js
+++ src/EventModal/eventmodal.js
@@ -9,12 +9,15 @@ export default class EventModal extends React.Component {
 
         let seldata = this.props.data[this.props.showindex]
 
+        let formatted_date = seldata.date.getFullYear() + '/' + (seldata.date.getMonth()+1) + '/' + seldata.date.getDate()
+
         return (
             <div className="modal-container">
+                <div className="modal-event-date">{formatted_date}</div>
                 <div className="modal-event-title">{seldata.title}</div>
                 <div className="modal-event-body">{seldata.body}</div>
                 <div className="modal-event-refs">
-                    {seldata.ref.map((d, i) => <span key={"refspan_" + i}>{d}</span>)}
+                    {seldata.ref.map((d, i) => <a key={"refspan_" + i} target="_blank" href={d}>{d}</a>)}
                 </div>
             </div>
         )
